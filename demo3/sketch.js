239f342c295f5611db8fa1cc5e9233fd09dca186
volis <volising@outlook.com>
2020-02-02 17:10:16 +0530

    final changes

--- demo3/sketch.js
+++ demo3/sketch.js
@@ -1,10 +1,14 @@
 var ctracker;
-var slider;
-var points_of_interest = [62];
+var left_eye = [27];
+var right_eye = [32];
+var videoInput;
+var lastFrameLeft;
+var lastFrameRight;
+let trail = [];
 
 function setup() {
   // setup camera capture
-  var videoInput = createCapture(VIDEO);
+  videoInput = createCapture(VIDEO);
   videoInput.size(800, 600);
   videoInput.position(0, 0);
   videoInput.hide();
@@ -17,25 +21,27 @@ function setup() {
   ctracker = new clm.tracker();
   ctracker.init(pModel);
   ctracker.start(videoInput.elt);
-
-  // define colors
-  noStroke();
-  fill('red')
 }
 
 function draw() {
-  image(capture, 0, 0, width, width * capture.height / capture.width);
-
+  image(videoInput, 0, 0, width, width * videoInput.height / videoInput.width);
+  
   // get array of face marker positions [x, y] format
   var positions = ctracker.getCurrentPosition();
   
   for (var i=0; i<positions.length; i++) {
     // check if are we interest in this point
-    if(points_of_interest.includes(i)) {
-      // write a large emoji at this location
-      textSize(250);
-      textAlign(CENTER);
-      text('🤩', positions[i][0], positions[i][1]);
+    if(left_eye.includes(i)) {
+        lastFrameLeft = get(positions[i][0] - 10, positions[i][1] - 10, 20, 20);
+        image(lastFrameLeft, positions[i][0], positions[i][1]);
+        image(lastFrameLeft, positions[i][0] + 5, positions[i][1] + 5);
+    }
+
+    /// 
+    if(right_eye.includes(i)) {
+        lastFrameRight = get(positions[i][0] - 10, positions[i][1] - 10, 20, 20);
+        image(lastFrameRight, positions[i][0], positions[i][1]);
+        image(lastFrameRight, positions[i][0] + 5, positions[i][1] + 5);
     }
   }
 }

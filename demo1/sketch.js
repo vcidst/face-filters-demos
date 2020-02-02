d51b85fb2668d2ae7f6b8b00bdf42a9bf0f6ed3c
vcidst <beingshailendra@gmail.com>
2020-02-02 10:56:57 +0530

    added mime types to html

--- demo1/sketch.js
+++ demo1/sketch.js
@@ -1,15 +1,20 @@
 var ctracker;
 var slider;
+var kite_length = 20;
+var kite_width = 10;
+var kite_tail = 5;
+var points_of_interest = [27, 32, 57];
+let trail = [];
+let a = 0;
 
 function setup() {
-
   // setup camera capture
   var videoInput = createCapture(0);
-  videoInput.size(400, 300);
+  videoInput.size(800, 600);
   videoInput.position(0, 0);
   
   // setup canvas
-  var cnv = createCanvas(400, 300);
+  var cnv = createCanvas(800, 600);
   cnv.position(0, 0);
 
   // setup tracker
@@ -24,16 +29,33 @@ function setup() {
 }
 
 function draw() {
-  clear();
-  
+  clear();		
+
   // get array of face marker positions [x, y] format
   var positions = ctracker.getCurrentPosition();
   
   for (var i=0; i<positions.length; i++) {
     // set the color of the ellipse based on position on screen
-    if (i == 32) {
-            fill(0);
-            ellipse(positions[i][0], positions[i][1], 80, 80);
+    if(1) { ///points_of_interest.includes(i)) {
+    	fill(map(positions[i][0], width*0.33, width*0.66, 0, 255), 255, map(positions[i][1], height*0.33, height*0.66, 0, 255));
+	    // draw ellipse at each position point
+	    //ellipse(positions[i][0], positions[i][1], 8, 8);
+
+	    triangle(positions[i][0], positions[i][1], 
+	    	positions[i][0] + kite_tail, positions[i][1] + kite_tail, 
+	    	positions[i][0] - kite_tail, positions[i][1] + kite_tail);
+	    
+	    triangle(positions[i][0], positions[i][1], 
+	    	positions[i][0] + kite_width, positions[i][1] - kite_width, 
+	    	positions[i][0] - kite_width, positions[i][1] - kite_width);
+	    
+	    triangle(positions[i][0], positions[i][1] - kite_length, 
+	    	positions[i][0] + kite_width, positions[i][1] - kite_width, 
+	    	positions[i][0] - kite_width, positions[i][1] - kite_width);
+
+	    trail.push([positions[i][0], positions[i][1]]);
     }
   }
+
+  
 }
